01d7fceb90a698ac3e929b94a3f6eb865297dcb6
localhorst87 <m.p.ahmann@gmx.de>
2021-05-15 13:02:23 +0200

    corrected missing deletion of offset argument

--- src/app/Businessrules/statistics-calculator.service.ts
+++ src/app/Businessrules/statistics-calculator.service.ts
@@ -3,8 +3,8 @@ import { Bet, Result, Match, Score, SeasonBet, SeasonResult } from './basic_data
 
 @Injectable()
 export abstract class StatisticsCalculatorService {
-  public abstract getScoreArray(matchArray: Match[], betArray: Bet[], resultArray: Result[], offset?: Score[]): Score[];
-  public abstract getSeasonScoreArray(betArray: SeasonBet[], resultArray: SeasonResult[], offset?: Score[]): Score[];
+  public abstract getScoreArray(matchArray: Match[], betArray: Bet[], resultArray: Result[]): Score[];
+  public abstract getSeasonScoreArray(betArray: SeasonBet[], resultArray: SeasonResult[]): Score[];
   public abstract addScores(score1: Score, score2: Score): Score;
   public abstract addScoreArrays(scoreArray1: Score[], ...scoreArray2: Score[][]): Score[];
   public abstract compareScores(firstEl: Score, secondEl: Score): number;

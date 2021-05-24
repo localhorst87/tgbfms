05167eca1612ee6720786841f9e9d5aed34644ba
localhorst87 <m.p.ahmann@gmx.de>
2021-05-24 14:52:54 +0200

    minor refactoring and added syncSeasonResult and

--- src/app/UseCases/synchronize-data.service.spec.ts
+++ src/app/UseCases/synchronize-data.service.spec.ts
@@ -2,8 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { AppdataAccessService } from '../Dataaccess/appdata-access.service';
 import { MatchdataAccessService } from '../Dataaccess/matchdata-access.service';
-import { MatchImportData } from '../Dataaccess/import_datastructures';
-import { Bet, Match, Result, Team } from '../Businessrules/basic_datastructures';
+import { MatchImportData, TeamRankingImportData } from '../Dataaccess/import_datastructures';
+import { Bet, Match, Result, Team, SeasonResult } from '../Businessrules/basic_datastructures';
 import { SynchronizeDataService } from './synchronize-data.service';
 import { of, from } from 'rxjs';
 import { defaultIfEmpty } from 'rxjs/operators';
@@ -15,8 +15,8 @@ describe('SynchronizeDataService', () => {
   let matchImportData: MatchImportData[];
 
   beforeEach(() => {
-    appDataSpy = jasmine.createSpyObj(["getMatch$", "addMatch", "updateMatch", "getResult$", "addResult", "updateResult"]);
-    matchDataSpy = jasmine.createSpyObj(["importMatchdata$"]);
+    appDataSpy = jasmine.createSpyObj(["getMatch$", "addMatch", "updateMatch", "getResult$", "addResult", "updateResult", "getSeasonResults$", "addSeasonResult", "updateSeasonResult"]);
+    matchDataSpy = jasmine.createSpyObj(["importMatchdata$", "importCurrentTeamRanking$"]);
 
     TestBed.configureTestingModule({
       providers: [
@@ -135,7 +135,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getMatch$.withArgs(argument2.matchId).and.returnValue(of(appDataMatch));
     appDataSpy.addMatch.and.stub();
     appDataSpy.updateMatch.and.stub();
-    spyOn<any>(service, "createMatch")
+    spyOn<any>(service, "convertToMatch")
       .withArgs(argument1, argument2, appDataMatch.isTopMatch).and.returnValue(transformedMatch);
     spyOn<any>(service, "isMatchEqual")
       .withArgs(transformedMatch, appDataMatch).and.returnValue(false);
@@ -176,7 +176,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getMatch$.withArgs(argument2.matchId).and.returnValue(of(appDataMatch));
     appDataSpy.addMatch.and.stub();
     appDataSpy.updateMatch.and.stub();
-    spyOn<any>(service, "createMatch")
+    spyOn<any>(service, "convertToMatch")
       .withArgs(argument1, argument2, appDataMatch.isTopMatch).and.returnValue(transformedMatch);
     spyOn<any>(service, "isMatchEqual")
       .withArgs(transformedMatch, appDataMatch).and.returnValue(false);
@@ -217,7 +217,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getMatch$.withArgs(argument2.matchId).and.returnValue(of(appDataMatch));
     appDataSpy.addMatch.and.stub();
     appDataSpy.updateMatch.and.stub();
-    spyOn<any>(service, "createMatch")
+    spyOn<any>(service, "convertToMatch")
       .withArgs(argument1, argument2, appDataMatch.isTopMatch).and.returnValue(transformedMatch);
     spyOn<any>(service, "isMatchEqual")
       .withArgs(transformedMatch, appDataMatch).and.returnValue(true);
@@ -234,10 +234,10 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getMatch$.withArgs(argument2.matchId).and.returnValue(of());
     appDataSpy.addMatch.and.stub();
     appDataSpy.updateMatch.and.stub();
-    spyOn<any>(service, "createMatch").and.callThrough();
+    spyOn<any>(service, "convertToMatch").and.callThrough();
 
     service["syncMatch"](argument1, argument2);
-    expect(service["createMatch"]).not.toHaveBeenCalled();
+    expect(service["convertToMatch"]).not.toHaveBeenCalled();
     expect(appDataSpy.updateMatch).not.toHaveBeenCalled();
     expect(appDataSpy.addMatch).not.toHaveBeenCalled();
   });
@@ -266,7 +266,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getResult$.withArgs(argument.matchId).and.returnValue(of(appdataResult));
     appDataSpy.addResult.and.stub();
     appDataSpy.updateResult.and.stub();
-    spyOn<any>(service, "createResult").withArgs(argument).and.returnValue(transformedResult);
+    spyOn<any>(service, "convertToResult").withArgs(argument).and.returnValue(transformedResult);
     spyOn<any>(service, "isResultEqual").withArgs(transformedResult, appdataResult).and.returnValue(false);
 
     service["syncResult"](argument);
@@ -294,7 +294,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getResult$.withArgs(argument.matchId).and.returnValue(of(appdataResult));
     appDataSpy.addResult.and.stub();
     appDataSpy.updateResult.and.stub();
-    spyOn<any>(service, "createResult").withArgs(argument).and.returnValue(transformedResult);
+    spyOn<any>(service, "convertToResult").withArgs(argument).and.returnValue(transformedResult);
     spyOn<any>(service, "isResultEqual").withArgs(transformedResult, appdataResult).and.returnValue(false);
 
     service["syncResult"](argument);
@@ -322,7 +322,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getResult$.withArgs(argument.matchId).and.returnValue(of(appdataResult));
     appDataSpy.addResult.and.stub();
     appDataSpy.updateResult.and.stub();
-    spyOn<any>(service, "createResult").withArgs(argument).and.returnValue(transformedResult);
+    spyOn<any>(service, "convertToResult").withArgs(argument).and.returnValue(transformedResult);
     spyOn<any>(service, "isResultEqual").withArgs(transformedResult, appdataResult).and.returnValue(true);
 
     service["syncResult"](argument);
@@ -350,7 +350,7 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getResult$.withArgs(argument.matchId).and.returnValue(of(appdataResult));
     appDataSpy.addResult.and.stub();
     appDataSpy.updateResult.and.stub();
-    spyOn<any>(service, "createResult").withArgs(argument).and.returnValue(transformedResult);
+    spyOn<any>(service, "convertToResult").withArgs(argument).and.returnValue(transformedResult);
     spyOn<any>(service, "isResultEqual").withArgs(transformedResult, appdataResult).and.returnValue(true);
 
     service["syncResult"](argument);
@@ -365,14 +365,490 @@ describe('SynchronizeDataService', () => {
     appDataSpy.getResult$.withArgs(argument.matchId).and.returnValue(of());
     appDataSpy.addResult.and.stub();
     appDataSpy.updateResult.and.stub();
-    spyOn<any>(service, "createResult").and.callThrough();
+    spyOn<any>(service, "convertToResult").and.callThrough();
     spyOn<any>(service, "isResultEqual").and.callThrough();
 
     service["syncResult"](argument);
-    expect(service["createResult"]).not.toHaveBeenCalled();
+    expect(service["convertToResult"]).not.toHaveBeenCalled();
     expect(service["isResultEqual"]).not.toHaveBeenCalled();
     expect(appDataSpy.addResult).not.toHaveBeenCalled();
     expect(appDataSpy.updateResult).not.toHaveBeenCalled();
   });
 
+  // ---------------------------------------------------------------------------
+  // syncSeasonResult
+  // ---------------------------------------------------------------------------
+
+  it('syncSeasonResult, import data available, app data not available', () => {
+    const argument1: number = 2020;
+    const argument2: TeamRankingImportData[] = [
+      {
+        teamId: 101,
+        matches: 34,
+        points: 80,
+        won: 25,
+        draw: 5,
+        lost: 4,
+        goals: 85,
+        goalsAgainst: 35
+      }, // 1
+      {
+        teamId: 102,
+        matches: 34,
+        points: 75,
+        won: 22,
+        draw: 9,
+        lost: 3,
+        goals: 60,
+        goalsAgainst: 30
+      }, // 2
+      {
+        teamId: 103,
+        matches: 34,
+        points: 66,
+        won: 20,
+        draw: 6,
+        lost: 8,
+        goals: 75,
+        goalsAgainst: 45
+      },
+      {
+        teamId: 104,
+        matches: 34,
+        points: 50,
+        won: 15,
+        draw: 5,
+        lost: 14,
+        goals: 50,
+        goalsAgainst: 41
+      },
+      {
+        teamId: 105,
+        matches: 34,
+        points: 48,
+        won: 14,
+        draw: 6,
+        lost: 14,
+        goals: 53,
+        goalsAgainst: 46
+      }, // -3
+      {
+        teamId: 106,
+        matches: 34,
+        points: 35,
+        won: 8,
+        draw: 9,
+        lost: 17,
+        goals: 43,
+        goalsAgainst: 56
+      }, // -2
+      {
+        teamId: 107,
+        matches: 34,
+        points: 29,
+        won: 5,
+        draw: 14,
+        lost: 15,
+        goals: 33,
+        goalsAgainst: 61
+      } // -1
+    ];
+
+    const convertedResults: SeasonResult[] = [
+      {
+        documentId: "",
+        season: argument1,
+        place: 1,
+        teamId: argument2[0].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: 2,
+        teamId: argument2[1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -1,
+        teamId: argument2[argument2.length - 1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -2,
+        teamId: argument2[argument2.length - 2].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -3,
+        teamId: argument2[argument2.length - 3].teamId
+      }
+    ];
+
+    appDataSpy.getSeasonResults$.and.returnValue(from([]));
+    appDataSpy.addSeasonResult.and.stub();
+    appDataSpy.updateSeasonResult.and.stub();
+
+    spyOn<any>(service, "convertToSeasonResult")
+      .withArgs(argument1, 1, argument2[0]).and.returnValue(convertedResults[0])
+      .withArgs(argument1, 2, argument2[1]).and.returnValue(convertedResults[1])
+      .withArgs(argument1, -1, argument2[argument2.length - 1]).and.returnValue(convertedResults[2])
+      .withArgs(argument1, -2, argument2[argument2.length - 2]).and.returnValue(convertedResults[3])
+      .withArgs(argument1, -3, argument2[argument2.length - 3]).and.returnValue(convertedResults[4]);
+
+    service["syncSeasonResult"](argument1, argument2);
+    expect(appDataSpy.addSeasonResult).toHaveBeenCalledTimes(5);
+    expect(appDataSpy.updateSeasonResult).not.toHaveBeenCalled();
+  });
+
+  it('syncSeasonResult, import data and app data available, but different', () => {
+    const argument1: number = 2020;
+    const argument2: TeamRankingImportData[] = [
+      {
+        teamId: 101,
+        matches: 34,
+        points: 80,
+        won: 25,
+        draw: 5,
+        lost: 4,
+        goals: 85,
+        goalsAgainst: 35
+      }, // 1
+      {
+        teamId: 102,
+        matches: 34,
+        points: 75,
+        won: 22,
+        draw: 9,
+        lost: 3,
+        goals: 60,
+        goalsAgainst: 30
+      }, // 2
+      {
+        teamId: 103,
+        matches: 34,
+        points: 66,
+        won: 20,
+        draw: 6,
+        lost: 8,
+        goals: 75,
+        goalsAgainst: 45
+      },
+      {
+        teamId: 104,
+        matches: 34,
+        points: 50,
+        won: 15,
+        draw: 5,
+        lost: 14,
+        goals: 50,
+        goalsAgainst: 41
+      },
+      {
+        teamId: 105,
+        matches: 34,
+        points: 48,
+        won: 14,
+        draw: 6,
+        lost: 14,
+        goals: 53,
+        goalsAgainst: 46
+      }, // -3
+      {
+        teamId: 106,
+        matches: 34,
+        points: 35,
+        won: 8,
+        draw: 9,
+        lost: 17,
+        goals: 43,
+        goalsAgainst: 56
+      }, // -2
+      {
+        teamId: 107,
+        matches: 34,
+        points: 29,
+        won: 5,
+        draw: 14,
+        lost: 15,
+        goals: 33,
+        goalsAgainst: 61
+      } // -1
+    ];
+
+    const convertedResults: SeasonResult[] = [
+      {
+        documentId: "",
+        season: argument1,
+        place: 1,
+        teamId: argument2[0].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: 2,
+        teamId: argument2[1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -1,
+        teamId: argument2[argument2.length - 1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -2,
+        teamId: argument2[argument2.length - 2].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -3,
+        teamId: argument2[argument2.length - 3].teamId
+      }
+    ];
+
+    const appdataResult: SeasonResult[] = [
+      {
+        documentId: "doc_first_place",
+        season: argument1,
+        place: 1,
+        teamId: 11
+      },
+      {
+        documentId: "doc_second_place",
+        season: argument1,
+        place: 2,
+        teamId: convertedResults[1].teamId
+      },
+      {
+        documentId: "doc_last_place",
+        season: argument1,
+        place: -1,
+        teamId: convertedResults[2].teamId
+      },
+      {
+        documentId: "doc_secondlast_place",
+        season: argument1,
+        place: -2,
+        teamId: 222
+      },
+      {
+        documentId: "doc_thirdlast_place",
+        season: argument1,
+        place: -3,
+        teamId: 333
+      }
+    ];
+
+    appDataSpy.getSeasonResults$.and.returnValue(from(appdataResult));
+    appDataSpy.addSeasonResult.and.stub();
+    appDataSpy.updateSeasonResult.and.stub();
+
+    spyOn<any>(service, "convertToSeasonResult")
+      .withArgs(argument1, 1, argument2[0]).and.returnValue(convertedResults[0])
+      .withArgs(argument1, 2, argument2[1]).and.returnValue(convertedResults[1])
+      .withArgs(argument1, -1, argument2[argument2.length - 1]).and.returnValue(convertedResults[2])
+      .withArgs(argument1, -2, argument2[argument2.length - 2]).and.returnValue(convertedResults[3])
+      .withArgs(argument1, -3, argument2[argument2.length - 3]).and.returnValue(convertedResults[4]);
+
+    service["syncSeasonResult"](argument1, argument2);
+    expect(appDataSpy.addSeasonResult).not.toHaveBeenCalled();
+    expect(appDataSpy.updateSeasonResult).toHaveBeenCalledWith(appdataResult[0].documentId, convertedResults[0]);
+    expect(appDataSpy.updateSeasonResult).not.toHaveBeenCalledWith(appdataResult[1].documentId, convertedResults[1]);
+    expect(appDataSpy.updateSeasonResult).not.toHaveBeenCalledWith(appdataResult[2].documentId, convertedResults[2]);
+    expect(appDataSpy.updateSeasonResult).toHaveBeenCalledWith(appdataResult[3].documentId, convertedResults[3]);
+    expect(appDataSpy.updateSeasonResult).toHaveBeenCalledWith(appdataResult[4].documentId, convertedResults[4]);
+  });
+
+  it('syncSeasonResult, import data and app data available and equal', () => {
+    const argument1: number = 2020;
+    const argument2: TeamRankingImportData[] = [
+      {
+        teamId: 101,
+        matches: 34,
+        points: 80,
+        won: 25,
+        draw: 5,
+        lost: 4,
+        goals: 85,
+        goalsAgainst: 35
+      }, // 1
+      {
+        teamId: 102,
+        matches: 34,
+        points: 75,
+        won: 22,
+        draw: 9,
+        lost: 3,
+        goals: 60,
+        goalsAgainst: 30
+      }, // 2
+      {
+        teamId: 103,
+        matches: 34,
+        points: 66,
+        won: 20,
+        draw: 6,
+        lost: 8,
+        goals: 75,
+        goalsAgainst: 45
+      },
+      {
+        teamId: 104,
+        matches: 34,
+        points: 50,
+        won: 15,
+        draw: 5,
+        lost: 14,
+        goals: 50,
+        goalsAgainst: 41
+      },
+      {
+        teamId: 105,
+        matches: 34,
+        points: 48,
+        won: 14,
+        draw: 6,
+        lost: 14,
+        goals: 53,
+        goalsAgainst: 46
+      }, // -3
+      {
+        teamId: 106,
+        matches: 34,
+        points: 35,
+        won: 8,
+        draw: 9,
+        lost: 17,
+        goals: 43,
+        goalsAgainst: 56
+      }, // -2
+      {
+        teamId: 107,
+        matches: 34,
+        points: 29,
+        won: 5,
+        draw: 14,
+        lost: 15,
+        goals: 33,
+        goalsAgainst: 61
+      } // -1
+    ];
+
+    const convertedResults: SeasonResult[] = [
+      {
+        documentId: "",
+        season: argument1,
+        place: 1,
+        teamId: argument2[0].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: 2,
+        teamId: argument2[1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -1,
+        teamId: argument2[argument2.length - 1].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -2,
+        teamId: argument2[argument2.length - 2].teamId
+      },
+      {
+        documentId: "",
+        season: argument1,
+        place: -3,
+        teamId: argument2[argument2.length - 3].teamId
+      }
+    ];
+
+    const appdataResult: SeasonResult[] = [
+      {
+        documentId: "doc_first_place",
+        season: argument1,
+        place: 1,
+        teamId: convertedResults[0].teamId
+      },
+      {
+        documentId: "doc_second_place",
+        season: argument1,
+        place: 2,
+        teamId: convertedResults[1].teamId
+      },
+      {
+        documentId: "doc_last_place",
+        season: argument1,
+        place: -1,
+        teamId: convertedResults[2].teamId
+      },
+      {
+        documentId: "doc_secondlast_place",
+        season: argument1,
+        place: -2,
+        teamId: convertedResults[3].teamId
+      },
+      {
+        documentId: "doc_thirdlast_place",
+        season: argument1,
+        place: -3,
+        teamId: convertedResults[4].teamId
+      }
+    ];
+
+    appDataSpy.getSeasonResults$.and.returnValue(from(appdataResult));
+    appDataSpy.addSeasonResult.and.stub();
+    appDataSpy.updateSeasonResult.and.stub();
+
+    spyOn<any>(service, "convertToSeasonResult")
+      .withArgs(argument1, 1, argument2[0]).and.returnValue(convertedResults[0])
+      .withArgs(argument1, 2, argument2[1]).and.returnValue(convertedResults[1])
+      .withArgs(argument1, -1, argument2[argument2.length - 1]).and.returnValue(convertedResults[2])
+      .withArgs(argument1, -2, argument2[argument2.length - 2]).and.returnValue(convertedResults[3])
+      .withArgs(argument1, -3, argument2[argument2.length - 3]).and.returnValue(convertedResults[4]);
+
+    service["syncSeasonResult"](argument1, argument2);
+    expect(appDataSpy.addSeasonResult).not.toHaveBeenCalled();
+    expect(appDataSpy.updateSeasonResult).not.toHaveBeenCalled();
+  });
+
+  it('syncSeasonResult, not enough import datasets', () => {
+    const argument1: number = 2020;
+    const argument2: TeamRankingImportData[] = [
+      {
+        teamId: 101,
+        matches: 34,
+        points: 80,
+        won: 25,
+        draw: 5,
+        lost: 4,
+        goals: 85,
+        goalsAgainst: 35
+      },
+      {
+        teamId: 102,
+        matches: 34,
+        points: 75,
+        won: 22,
+        draw: 9,
+        lost: 3,
+        goals: 60,
+        goalsAgainst: 30
+      }
+    ];
+
+    service["syncSeasonResult"](argument1, argument2);
+    expect(appDataSpy.getSeasonResults$).not.toHaveBeenCalled();
+    expect(appDataSpy.addSeasonResult).not.toHaveBeenCalled();
+    expect(appDataSpy.updateSeasonResult).not.toHaveBeenCalled();
+  });
 });
--- src/app/UseCases/synchronize-data.service.ts
+++ src/app/UseCases/synchronize-data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { take } from 'rxjs/operators';
+import { take, toArray } from 'rxjs/operators';
 import { AppdataAccessService } from '../Dataaccess/appdata-access.service';
 import { MatchdataAccessService } from '../Dataaccess/matchdata-access.service';
-import { MatchImportData } from '../Dataaccess/import_datastructures';
-import { Bet, Match, Result, Team } from '../Businessrules/basic_datastructures';
+import { MatchImportData, TeamRankingImportData } from '../Dataaccess/import_datastructures';
+import { Bet, Match, Result, Team, SeasonResult } from '../Businessrules/basic_datastructures';
+
+const RELEVANT_FIRST_PLACES_COUNT: number = 2; // indicates number of first places of season bets to evaluate
+const RELEVANT_LAST_PLACES_COUNT: number = 3; // indicates number of last places of season bets to evaluate
 
 @Injectable({
   providedIn: 'root'
@@ -31,7 +34,7 @@ export class SynchronizeDataService {
     this.appDataAccess.getMatch$(importedData.matchId).pipe(take(1)).subscribe(
       (appdataMatch: Match) => {
 
-        let importedMatch: Match = this.createMatch(season, importedData, appdataMatch.isTopMatch);
+        let importedMatch: Match = this.convertToMatch(season, importedData, appdataMatch.isTopMatch);
 
         if (appdataMatch.documentId == "") { // match not yet exported to database
           this.appDataAccess.addMatch(importedMatch);
@@ -50,7 +53,7 @@ export class SynchronizeDataService {
     this.appDataAccess.getResult$(importedData.matchId).pipe(take(1)).subscribe(
       (appdataResult: Result) => {
 
-        let importedResult: Result = this.createResult(importedData);
+        let importedResult: Result = this.convertToResult(importedData);
 
         if (importedResult.goalsHome >= 0 && importedResult.goalsAway >= 0) { // result is available
 
@@ -67,7 +70,68 @@ export class SynchronizeDataService {
     );
   }
 
-  private createMatch(season: number, importedData: MatchImportData, isTopMatch: boolean = false): Match {
+  private syncSeasonResult(season: number, importedData: TeamRankingImportData[]): void {
+    // synchronizes SeasonResults in app database
+
+    const numRequiredResults: number = RELEVANT_FIRST_PLACES_COUNT + RELEVANT_LAST_PLACES_COUNT;
+    const nImports: number = importedData.length;
+
+    if (nImports < numRequiredResults) { // not enough data available?
+      return;
+    }
+
+    this.appDataAccess.getSeasonResults$(season).pipe(toArray()).subscribe(
+      (appdataResults: SeasonResult[]) => {
+
+        // first places
+        for (let i = 1; i <= RELEVANT_FIRST_PLACES_COUNT; i++) {
+
+          let importedResult: SeasonResult = this.convertToSeasonResult(season, i, importedData[i - 1]);
+          let appdataResult: SeasonResult[] = appdataResults.filter(result => result.place == i);
+
+          if (appdataResult.length == 0) { // SeasonResult not availabe in app database
+            this.appDataAccess.addSeasonResult(importedResult);
+            continue;
+          }
+
+          if (!this.isSeasonResultEqual(appdataResult[0], importedResult)) { // available, but not up to date
+            this.appDataAccess.updateSeasonResult(appdataResult[0].documentId, importedResult);
+          }
+        }
+
+        // last places
+        for (let i = -1; i >= -RELEVANT_LAST_PLACES_COUNT; i--) {
+
+          // in case of 18 teams, -1 is the 18th place, -2 is the 17th place, ...
+          let importedResult: SeasonResult = this.convertToSeasonResult(season, i, importedData[i + nImports]);
+          let appdataResult: SeasonResult[] = appdataResults.filter(result => result.place == i);
+
+          if (appdataResult.length == 0) { // SeasonResult not availabe in app database
+            this.appDataAccess.addSeasonResult(importedResult);
+            continue;
+          }
+
+          if (!this.isSeasonResultEqual(appdataResult[0], importedResult)) { // available, but not up to date
+            this.appDataAccess.updateSeasonResult(appdataResult[0].documentId, importedResult);
+          }
+        }
+
+      }
+    );
+  }
+
+  private convertToSeasonResult(season: number, place: number, teamRankingRow: TeamRankingImportData) {
+    //
+
+    return {
+      documentId: "",
+      season: season,
+      place: place,
+      teamId: teamRankingRow.teamId
+    };
+  }
+
+  private convertToMatch(season: number, importedData: MatchImportData, isTopMatch: boolean = false): Match {
     // creates a new Match structure for writing to the app database
 
     return {
@@ -83,7 +147,7 @@ export class SynchronizeDataService {
     };
   }
 
-  private createResult(importedData: MatchImportData): Result {
+  private convertToResult(importedData: MatchImportData): Result {
     // creates a new Result structure for writing to the app database
 
     return {
@@ -94,25 +158,33 @@ export class SynchronizeDataService {
     };
   }
 
-  private isMatchEqual(match: Match, matchExt: Match): boolean {
+  private isMatchEqual(match1: Match, match2: Match): boolean {
     // checks if all properties are equal and leaves out the document ID !
 
-    return (match.season == matchExt.season &&
-      match.matchday == matchExt.matchday &&
-      match.matchId == matchExt.matchId &&
-      match.timestamp == matchExt.timestamp &&
-      match.isFinished == matchExt.isFinished &&
-      match.isTopMatch == matchExt.isTopMatch &&
-      match.teamIdHome == matchExt.teamIdHome &&
-      match.teamIdAway == matchExt.teamIdAway);
+    return (match1.season == match2.season &&
+      match1.matchday == match2.matchday &&
+      match1.matchId == match2.matchId &&
+      match1.timestamp == match2.timestamp &&
+      match1.isFinished == match2.isFinished &&
+      match1.isTopMatch == match2.isTopMatch &&
+      match1.teamIdHome == match2.teamIdHome &&
+      match1.teamIdAway == match2.teamIdAway);
   }
 
-  private isResultEqual(result: Result, resultExt: Result): boolean {
+  private isResultEqual(result1: Result, result2: Result): boolean {
     // checks if all properties are equal and leaves out the document ID !
 
-    return (result.matchId == resultExt.matchId &&
-      result.goalsHome == resultExt.goalsHome &&
-      result.goalsAway == resultExt.goalsAway);
+    return (result1.matchId == result2.matchId &&
+      result1.goalsHome == result2.goalsHome &&
+      result1.goalsAway == result2.goalsAway);
+  }
+
+  private isSeasonResultEqual(result1: SeasonResult, result2: SeasonResult): boolean {
+    // checks if all properties are equal, except the document ID
+
+    return (result1.season == result2.season &&
+      result1.place == result2.place &&
+      result1.teamId == result2.teamId);
   }
 
 }

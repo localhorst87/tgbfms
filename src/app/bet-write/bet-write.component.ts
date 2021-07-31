ae7dd5fd86972247b1dd0aefaef825f3e9e7b6e4
localhorst87 <m.p.ahmann@gmx.de>
2021-07-31 14:02:34 +0200

    integrated onChanges for initial matchday selection + did some renaming

--- src/app/bet-write/bet-write.component.ts
+++ src/app/bet-write/bet-write.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { FetchBetWriteDataService } from '../UseCases/fetch-bet-write-data.service';
 import { FetchBasicDataService } from '../UseCases/fetch-basic-data.service';
 import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
@@ -12,60 +12,70 @@ import { SEASON, MATCHDAYS_PER_SEASON, NUMBER_OF_TEAMS } from '../Businessrules/
   templateUrl: './bet-write.component.html',
   styleUrls: ['./bet-write.component.css']
 })
-export class BetWriteComponent implements OnInit {
+export class BetWriteComponent implements OnInit, OnChanges {
 
   @Input() userId: string;
-  nTeams: number;
-  matchdayForm: FormControl;
-  durationForm: FormControl;
-  matches: BetWriteData[];
-  seasonBets: SeasonBetWriteData[];
-  dForm: FormGroup;
-  seasonBetForm: FormGroup;
-  displayMethodForm: FormControl;
+  nTeams: number; // total number of teams in the campaign
+  nMatchdays: number; // total number of matchdays per season
+  activeTeams: Team[]; // all active teams in this campaign
+  matches: BetWriteData[]; // loaded matches to bet
+  seasonBets: SeasonBetWriteData[]; // loaded season bets
   isPanelExpanded: boolean;
-  selectedDisplayMethod: string;
-  selectedMatchday: number;
-  selectedDuration: number;
-  activeTeams: Team[];
+  displayMethodForm: FormControl; // option field (matchday, duration, season)
+  selectedDisplayMethod: string; // identifier of selected option
+  matchdayForm: FormControl; // slider which matchday to load
+  @Input() selectedMatchday: number; // pre allocated with next matchday
+  durationForm: FormControl; // slider of future days matches to load
+  selectedDuration: number; // pre allocated with 7 days
+  betForm: FormGroup; // formular for setting bets
+  seasonBetForm: FormGroup; // formular for setting season bets
 
   constructor(
     private fetchBetService: FetchBetWriteDataService,
     private fetchBasicService: FetchBasicDataService,
-    private acc: AppdataAccessService,
-    private fb: FormBuilder) {
+    private appData: AppdataAccessService,
+    private formBuilder: FormBuilder) {
 
     this.userId = "";
     this.nTeams = NUMBER_OF_TEAMS;
+    this.nMatchdays = MATCHDAYS_PER_SEASON;
+    this.selectedMatchday = 1;
     this.matches = [];
     this.seasonBets = [];
-    this.dForm = this.fb.group({
-      bets: this.fb.array([])
+    this.betForm = this.formBuilder.group({
+      bets: this.formBuilder.array([])
     });
-    this.seasonBetForm = this.fb.group({
-      places: this.fb.array([])
+    this.seasonBetForm = this.formBuilder.group({
+      places: this.formBuilder.array([])
     });
-    this.displayMethodForm = this.fb.control("matchday");
+    this.displayMethodForm = this.formBuilder.control("matchday");
     this.isPanelExpanded = false;
     this.selectedDisplayMethod = "matchday";
-    this.selectedMatchday = 1;
-    this.matchdayForm = this.fb.control(this.selectedMatchday);
+    this.matchdayForm = this.formBuilder.control(this.selectedMatchday);
     this.selectedDuration = 7;
-    this.durationForm = this.fb.control(this.selectedDuration);
+    this.durationForm = this.formBuilder.control(this.selectedDuration);
     this.activeTeams = [];
     this.fetchBasicService.fetchActiveTeams$(SEASON).subscribe(
       (team: Team) => this.activeTeams.push(team)
     );
   }
 
+  get bets() {
+    return this.betForm.get('bets') as FormArray;
+  }
+
+  get places() {
+    return this.seasonBetForm.get('places') as FormArray;
+  }
+
   resetData(): void {
     this.matches = [];
     this.seasonBets = [];
-    this.dForm = this.fb.group({
-      bets: this.fb.array([])
+    this.betForm = this.formBuilder.group({
+      bets: this.formBuilder.array([])
     });
-    this.seasonBetForm = this.fb.group({
-      places: this.fb.array([])
+    this.seasonBetForm = this.formBuilder.group({
+      places: this.formBuilder.array([])
     });
   }
 
@@ -108,28 +118,20 @@ export class BetWriteComponent implements OnInit {
     this.selectedDisplayMethod = "season";
   }
 
-  get bets() {
-    return this.dForm.get('bets') as FormArray;
-  }
-
-  get places() {
-    return this.seasonBetForm.get('places') as FormArray;
-  }
-
   addMatchForm(betWriteData: BetWriteData): void {
     // adds the BetWriteData to the Form
 
-    let matchForm: FormGroup = this.fb.group({
+    let matchForm: FormGroup = this.formBuilder.group({
       betHome: [{ value: betWriteData.betGoalsHome < 0 ? '' : betWriteData.betGoalsHome, disabled: betWriteData.isBetFixed }, Validators.min(0)],
       betAway: [{ value: betWriteData.betGoalsAway < 0 ? '' : betWriteData.betGoalsAway, disabled: betWriteData.isBetFixed }, Validators.min(0)]
     });
 
-    this.subscribeCorrectingValues(matchForm);
-    this.subscribeToChanges(matchForm, betWriteData);
+    this.subscribeCorrectingBetValues(matchForm);
+    this.subscribeToBetChanges(matchForm, betWriteData);
     this.bets.push(matchForm);
   }
 
-  subscribeToChanges(form: FormGroup, betData: BetWriteData) {
+  subscribeToBetChanges(form: FormGroup, betData: BetWriteData) {
     // refreshes the Bet in the App Database upon checking the values
 
     form.valueChanges.subscribe(value => {
@@ -143,19 +145,19 @@ export class BetWriteComponent implements OnInit {
         goalsAway: value.betAway
       };
 
-      if (this.isFormFilled(form)) {
-        this.acc.setBet(updatedBet);
+      if (this.isBetFormFilled(form)) {
+        this.appData.setBet(updatedBet);
       }
     });
   }
 
-  isFormFilled(form: FormGroup): boolean {
+  isBetFormFilled(form: FormGroup): boolean {
     // checks if home AND away fields are filled
 
     return form.controls.betHome.value != null && form.controls.betAway.value != null;
   }
 
-  subscribeCorrectingValues(form: FormGroup): void {
+  subscribeCorrectingBetValues(form: FormGroup): void {
     // corrects any invalid value to zero
 
     form.controls.betHome.statusChanges.subscribe(status => {
@@ -167,6 +169,36 @@ export class BetWriteComponent implements OnInit {
     form.controls.betAway.statusChanges.subscribe(status => {
       if (status == "INVALID") {
         form.controls.betAway.setValue(0);
+      }
+    });
+  }
+
+  addSeasonBetForm(seasonBetWriteData: SeasonBetWriteData): void {
+    // is adding SeasonBetWriteData to the Form
+
+    let placeForm: FormControl = this.formBuilder.control({ value: String(seasonBetWriteData.teamId), disabled: seasonBetWriteData.isBetFixed });
+
+    this.subscribeToSeasonBetChanges(placeForm, seasonBetWriteData);
+    this.places.push(placeForm);
+  }
+
+  subscribeToSeasonBetChanges(form: FormControl, seasonData: SeasonBetWriteData): void {
+    // refreshes the SeasonBet in the App Database upon checking the values
+
+    form.valueChanges.subscribe(value => {
+      if (this.isSeasonBetSelectionValid() && value != undefined) {
+        let updatedBet: SeasonBet = {
+          documentId: seasonData.betDocumentId,
+          season: seasonData.season,
+          userId: this.userId,
+          isFixed: seasonData.isBetFixed,
+          place: seasonData.place,
+          teamId: Number(value)
+        };
+        this.appData.setSeasonBet(updatedBet);
+      }
+      else {
+        form.setValue("-1");
       }
     });
   }
@@ -211,8 +243,8 @@ export class BetWriteComponent implements OnInit {
       goalsAway: form.controls.betAway.value
     };
 
-    if (this.isFormFilled(form)) {
-      this.acc.setBet(updatedBet); // updated in App Database
+    if (this.isBetFormFilled(form)) {
+      this.appData.setBet(updatedBet); // updated in App Database
       this.matches[iBet].isBetFixed = true; // refreshes BetWriteData
       this.bets.controls[iBet].disable(); // disable input form
     }
@@ -240,38 +272,19 @@ export class BetWriteComponent implements OnInit {
         place: this.seasonBets[iBet].place,
         teamId: newTeamId
       };
-      this.acc.setSeasonBet(updatedBet);
+      this.appData.setSeasonBet(updatedBet);
       this.seasonBets[iBet].isBetFixed = true;
       this.places.controls[iBet].disable();
     }
   }
 
-  addSeasonBetForm(seasonBetWriteData: SeasonBetWriteData): void {
-    // is adding SeasonBetWriteData to the Form
+  ngOnInit(): void {
 
-    let placeForm: FormControl = this.fb.control({ value: String(seasonBetWriteData.teamId), disabled: seasonBetWriteData.isBetFixed });
-
-    placeForm.valueChanges.subscribe(value => {
-      if (this.isSeasonBetSelectionValid() && value != undefined) {
-        let updatedBet: SeasonBet = {
-          documentId: seasonBetWriteData.betDocumentId,
-          season: seasonBetWriteData.season,
-          userId: this.userId,
-          isFixed: seasonBetWriteData.isBetFixed,
-          place: seasonBetWriteData.place,
-          teamId: Number(value)
-        };
-        this.acc.setSeasonBet(updatedBet);
-      }
-      else {
-        placeForm.setValue("-1");
-      }
-    });
-
-    this.places.push(placeForm);
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.matchdayForm.setValue(this.selectedMatchday);
+    this.showMatchesByMatchday(this.selectedMatchday);
   }
 
 }

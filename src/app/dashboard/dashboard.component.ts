c7fbeefa982c7465540025f5d33c3773bdc6e3d1
localhorst87 <m.p.ahmann@gmx.de>
2021-10-24 20:35:20 +0200

    corrected main area size of dashboard. it does not work out as expected

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -160,9 +160,6 @@ export class DashboardComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
 
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
     // refresh current time
     interval(INTERVAL_TIME_REFRESH).subscribe(
       val => {

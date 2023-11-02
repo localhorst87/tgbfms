e58065ade5bf4090fe6546a1cd9cc1ef2ec63634
localhorst87 <m.p.ahmann@gmx.de>
2023-11-02 22:55:51 +0100

    added progress bar and auto close to user-profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -4,7 +4,6 @@ import { FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from '../Businessrules/basic_datastructures';
 import { SimpleResult } from '../Dataaccess/import_datastructures';
-import { AppdataAccessService } from '../Dataaccess/appdata-access.service';
 
 const PW_MIN_LENGTH: number = 6;
 
@@ -23,10 +22,15 @@ export class UserProfileComponent implements OnInit {
   hidePassword: boolean;
   username: FormControl;
 
+  isLoading: boolean;
+
+  isEmailExpanded: boolean;
+  isPasswordExpanded: boolean;
+  isUsernameExpanded: boolean;
+
   constructor(
     private functions: AngularFireFunctions,
-    private snackbar: MatSnackBar,
-    private appdata: AppdataAccessService) {
+    private snackbar: MatSnackBar) {
     
     this.user = {
       documentId: "",
@@ -49,9 +53,17 @@ export class UserProfileComponent implements OnInit {
     this.hidePassword = true;
 
     this.username = new FormControl("");
+
+    this.isLoading = false;
+
+    this.isEmailExpanded = false;
+    this.isPasswordExpanded = false;
+    this.isUsernameExpanded = false;
   }
 
   changeEmail(): void {
+    this.isLoading = true;
+
     const callable = this.functions.httpsCallable('changeEmail');
     callable({ userId: this.user.id, newEmail: this.email.value }).subscribe(
       (result: SimpleResult) => {
@@ -59,17 +71,22 @@ export class UserProfileComponent implements OnInit {
           let snackbarMsg: string = "Email zur Bestätigung an deine neue Email versendet.";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
+          this.isEmailExpanded = false;
         }
         else {
           let snackbarMsg: string = "Da ist was schief gelaufen. Versuche es später nochmal...";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
         }
+
+        this.isLoading = false;
       }
     );
   }
 
   changePassword(): void {
+    this.isLoading = true;
+
     const callable = this.functions.httpsCallable('changePassword');
     callable({ userId: this.user.id, newPassword: this.password.value }).subscribe(
       (result: SimpleResult) => {
@@ -77,17 +94,22 @@ export class UserProfileComponent implements OnInit {
           let snackbarMsg: string = "Passwort wurde geändert.";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
+          this.isPasswordExpanded = false;
         }
         else {
           let snackbarMsg: string = "Da ist was schief gelaufen. Versuche es später nochmal...";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
         }
+
+        this.isLoading = false;
       }
     )
   }
 
   changeUsername(): void {
+    this.isLoading = true;
+
     const callable = this.functions.httpsCallable('changeUsername');
     callable({ oldUsername: this.user.displayName, newUsername: this.username.value }).subscribe(
       (result: SimpleResult) => {
@@ -95,12 +117,15 @@ export class UserProfileComponent implements OnInit {
           let snackbarMsg: string = "Anforderung zur Änderung wurde gesendet";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
+          this.isUsernameExpanded = false;
         }
         else {
           let snackbarMsg: string = "Da ist was schief gelaufen. Versuche es später nochmal...";
           let confirmMsg: string = "OK";
           this.snackbar.open(snackbarMsg, confirmMsg);
         }
+
+        this.isLoading = false;
       }
     )
   }
